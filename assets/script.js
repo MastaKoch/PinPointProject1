bd794f87f82884737c8ecf32814a62f73138ba64
lundyc0917 <lundyc0917@gmail.com>
2020-12-08 12:22:02 -0500

    centered buttons and search bar

--- assets/script.js
+++ assets/script.js
@@ -42,8 +42,9 @@ $("#searchBtn").on("click", function(event){
 });
 
 
-// cityPresent function.. shows the weather forecast at the present moment.
+// weatherPresent function.. shows the weather forecast at the present moment.
 function weatherPresent(){
+
   const weatherSettings = {
     "url": "https://api.openweathermap.org/data/2.5/weather?q=" +citySearch+ "&units=imperial&appid=" + APIKey,
     "method": "GET"
@@ -51,23 +52,26 @@ function weatherPresent(){
     
   $.ajax(weatherSettings).done(function (response) {
     console.log(response);
-    
-    var temperature= $("<p>").text("Temperature: "+response.main.temp+"F");
-    var humidity = $("<p>").text("Humidity: "+response.main.humidity);
-    $("#cityReturn").empty();
-    $("#temp").empty();
-    $("#humidity").empty();
 
-    $("#cityReturn").append(citySearch);
-    $("#temp").append(temperature);
-    $("#humidity").append(humidity);
+    var temperature=response.main.temp;
+    var humidity=response.main.humidity;
+    $("#weatherCity").html("<h1>"+citySearch+"</h1>");
+
+    var weatherData = "<ul>"
+    weatherData += "<p>Temperature: "+temperature+"</p>";
+    weatherData += "<p>Humidity: "+humidity+"</p>";
+    $("#weatherReturn").html(weatherData);
     
   });
 }
 
 
 // Hotel data API
+
+// get city ID and set it to session storage to be used in hotelPresent function
 function getCityId(){
+
+  // settings for ajax function
   const citySearchSettings = {
     "async": false,
     "crossDomain": true,
@@ -79,73 +83,74 @@ function getCityId(){
     }
   };
   
+  // ajax to call response from input
   $.ajax(citySearchSettings).done(function (response) {
     console.log(response);
     var cityId = response.suggestions[0].entities[0].destinationId;
+    // add value to sessionStorage to be able to call it in a later function outside of ajax
     sessionStorage.setItem("cityId", response.suggestions[0].entities[0].destinationId);
     console.log(cityId);
   });
 }
 
-
+// function to return hotel data
 function hotelPresent(){
-  // var destId = sessionStorage.getItem("cityId");
-  var destId = "1528090";
-  // console.log(cityId);
+  // Pull cityId from the session storage written in getCityId function
+  var destId = sessionStorage.getItem("cityId");
+  // var destId = "1528090";
   var pageNumber = 1;
-var checkIn = "2020-12-08";
-var checkOut = "2020-12-15";
-var pageSize=5;
-var adults1=1;
+  var checkIn = "2020-12-08";
+  var checkOut = "2020-12-15";
+  var pageSize=5;
+  var adults1=1;
 
- const settings = {
-	"async": false,
-	"crossDomain": true,
-	"url": "https://hotels4.p.rapidapi.com/properties/list?destinationId="+destId+"&pageNumber="+pageNumber+"&checkIn="+checkIn+"&checkOut="+checkOut+"&pageSize="+pageSize+"&adults1="+adults1+"&currency=USD&locale=en_US&sortOrder=PRICE",
-	// "url":"https://hotels4.p.rapidapi.com/properties/list?destinationId=1506246&pageNumber=1&checkIn=2020-01-08&checkOut=2020-01-15&pageSize=25&adults1=1&currency=USD&locale=en_US&sortOrder=PRICE",
-	"method": "GET",
-	"headers": {
-	"x-rapidapi-key": "8b8ddf2d71mshf03a9aae2c1ec77p127802jsn64b1b332d9eb",
-	"x-rapidapi-host": "hotels4.p.rapidapi.com"
-	}
+  // settings for ajax function
+  const settings = {
+    "async": false,
+    "crossDomain": true,
+    "url": "https://hotels4.p.rapidapi.com/properties/list?destinationId="+destId+"&pageNumber="+pageNumber+"&checkIn="+checkIn+"&checkOut="+checkOut+"&pageSize="+pageSize+"&adults1="+adults1+"&currency=USD&locale=en_US&sortOrder=PRICE",
+    // "url":"https://hotels4.p.rapidapi.com/properties/list?destinationId=1506246&pageNumber=1&checkIn=2020-01-08&checkOut=2020-01-15&pageSize=25&adults1=1&currency=USD&locale=en_US&sortOrder=PRICE",
+    "method": "GET",
+    "headers": {
+      "x-rapidapi-key": "8b8ddf2d71mshf03a9aae2c1ec77p127802jsn64b1b332d9eb",
+      "x-rapidapi-host": "hotels4.p.rapidapi.com"
+    }
 
-};
+  };
 
-// function getRating(obj){
-// 	if (obj.guestReviews){
-// 		return obj.guestReviews.rating; 
-// 	}
-// 	return "";
-// }
-$.ajax(settings).done(function (response) {
-	console.log(response);
-	//display query value
-	var destination = response.data.body.query.destination.value;
-	$("#query-info").html("<h1>"+destination+"</h1>");
+  // ajax to call response from input
+  $.ajax(settings).done(function (response) {
+    console.log(response);
+    //display query value
+    // var destination = response.data.body.query.destination.value;
 
-	//display search result
-	var result = response.data.body.searchResults.results.length;
-	var hotelList = "<ul>"
-	for(i=0; i<pageSize; i++){
-		//var reviewRating = response.data.body.searchResults.results[i].guestReviews.rating;
-		//var reviewRaging = getRating(response.data.body.results[i]);
-		//var reviewScale = response.data.body.searchResults.results[i].guestReviews.scale;
-		//var reviewTotal = response.data.body.searchResults.results[i].guestReviews.total;
-		console.log(i);
-		var hotelName = response.data.body.searchResults.results[i].name;
-		var hotelStreetAddress = response.data.body.searchResults.results[i].address.streetAddress;
-		var pricePerNight = response.data.body.searchResults.results[i].ratePlan.price.current;
-		var totalPrice = response.data.body.searchResults.results[i].ratePlan.price.totalPricePerStay;
-		var thumbnailUrl = response.data.body.searchResults.results[i].thumbnailUrl;
-		hotelList += "<li><h2>Hotel Name: "+hotelName+"</h2></br>";
-		hotelList += "<div class='div-left'><img src='" + thumbnailUrl+"'></img></div>\
-                 </li>";
-    hotelList += "<div><p>"+ hotelStreetAddress + "</p></div>";
-		
-	}
+    // write data back to html
+    $("#hotelCity").html("<h1>"+citySearch+"</h1>");
 
-	$("#result").html(hotelList);
+    //display search result
+    var result = response.data.body.searchResults.results.length;
+    var hotelList = "<ul>"
+    for(i=0; i<pageSize; i++){
+      //var reviewRating = response.data.body.searchResults.results[i].guestReviews.rating;
+      //var reviewRaging = getRating(response.data.body.results[i]);
+      //var reviewScale = response.data.body.searchResults.results[i].guestReviews.scale;
+      //var reviewTotal = response.data.body.searchResults.results[i].guestReviews.total;
+      console.log(i);
+      var hotelName = response.data.body.searchResults.results[i].name;
+      var hotelStreetAddress = response.data.body.searchResults.results[i].address.streetAddress;
+      var hotelLocality = response.data.body.searchResults.results[i].address.locality;
+      var hotelRegion = response.data.body.searchResults.results[i].address.region;
+      var hotelPostalCode = response.data.body.searchResults.results[i].address.postalCode;
+      var pricePerNight = response.data.body.searchResults.results[i].ratePlan.price.current;
+      var totalPrice = response.data.body.searchResults.results[i].ratePlan.price.totalPricePerStay;
+      var thumbnailUrl = response.data.body.searchResults.results[i].thumbnailUrl;
+      hotelList += "<li><h2>Hotel Name: "+hotelName+"</h2></br>";
+      hotelList += "<div class='div-left'><img src='" + thumbnailUrl+"'></img>"
+      hotelList += "<p id='address'>"+ hotelStreetAddress + "<br>" +hotelLocality+", "+hotelRegion+" "+hotelPostalCode+"</p></div></li>";
+      
+    }
+    // write data back to html
+    $("#hotelReturn").html(hotelList);
 
-
-});
+  });
 }
